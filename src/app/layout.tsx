51def79ede5b4c5898a8572596cc06c2138d7c55
Rui Dias <rui.dias@five9.com>
2025-04-18 15:50:53 +0100

    testing imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import Header from '@/components/header/header';
+import Header from '../components/header/header';
 import FooterMobile from '@/components/footer/FooterMobile';
 
 const geistSans = Geist({
