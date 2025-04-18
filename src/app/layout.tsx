3b4baf74430cf123aaa56a30337f31cbce138fcc
Rui Dias <rui.dias@five9.com>
2025-04-18 16:05:33 +0100

    updated imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import FooterMobile from '@/components/footer/FooterMobile';
-import Header from '@/components/header/Header';
+import Header from '@/components/header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
