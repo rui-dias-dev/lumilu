8de381d59beb10fdb7a8f7d960d90d3691dcee9b
Rui Dias <rui.dias@five9.com>
2025-07-27 13:57:44 +0100

    build: added meta link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import PixelTracker from '@/components/PixelTracker';
 
 const montserratSans = Montserrat({
   variable: '--font-montserrat-sans',
@@ -22,8 +23,37 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleAnalytics gaId="G-EBC8YLMLP3" />
+      <head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1510293559956799');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1510293559956799&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body className={`${montserratSans.className} border-grid bg-cream flex flex-1 flex-col antialiased`}>
+        <GoogleAnalytics gaId="G-EBC8YLMLP3" />
+        <PixelTracker />
         <Header />
         {children}
         <Footer />
---
+++ src/components/PixelTracker.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import ReactPixel from 'react-facebook-pixel';
+
+const PixelTracker = () => {
+  useEffect(() => {
+    const pixelId = '1510293559956799';
+    ReactPixel.init(pixelId);
+    ReactPixel.pageView();
+  }, []);
+  return null;
+};
+export default PixelTracker;
